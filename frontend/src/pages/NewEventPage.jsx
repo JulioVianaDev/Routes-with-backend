22f0052c31ab7ecee3da2e3c2fabe3b2b63c9a86
juliovianadev <juliovianadev@gmail.com>
2023-03-13 18:33:38 -0300

    using data

--- frontend/src/pages/NewEventPage.jsx
+++ frontend/src/pages/NewEventPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import EventForm from '../components/EventForm'
 function NewEventPage() {
-  return (
-    <div>NewEventPage</div>
-  )
+  // function SubmitionData(event){
+  //   event.preventDefault();
+  // }
+  return <EventForm/>
 }
 
 export default NewEventPage
