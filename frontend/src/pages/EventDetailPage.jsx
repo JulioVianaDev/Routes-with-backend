b23a5a4f6dc0c6d3d7b209b2581ffc83d6c80c50
juliovianadev <juliovianadev@gmail.com>
2023-03-23 00:30:22 -0300

    loading events

--- frontend/src/pages/EventDetailPage.jsx
+++ frontend/src/pages/EventDetailPage.jsx
@@ -1,30 +1,67 @@
-import React from 'react'
-import {redirect, useRouteLoaderData ,json} from 'react-router-dom'
+import React,{Suspense} from 'react'
+import {redirect, useRouteLoaderData ,json,defer,Await} from 'react-router-dom'
 import EventItem from '../components/EventItem.js'
-
+import EventsList from '../components/EventsList.js'
 function EventDetailPage() {
-  const data= useRouteLoaderData('event-detail');
+  const {event,events}= useRouteLoaderData('event-detail');
   return (
     <>
-      <EventItem event={data.event}/>
+    <Suspense fallback={<p>carregando evento</p>}>
+      <Await resolve={event}>
+        {(loadEvent)=> <EventItem event={loadEvent}/>}
+        <EventItem event={event}/>
+      </Await>
+    </Suspense>
+    <Suspense fallback={<p>carregando eventos</p>}>
+      <Await resolve={events}>
+        {(loadEvents)=><EventsList events={loadEvents}/>}
+      </Await>
+    </Suspense>
     </>
   )
 }
 
 export default EventDetailPage
 
+async function loadEvent(id) {
+  const response = await fetch('http://localhost:8080/events/'+id);
+  if (!response.ok){
+    throw json(
+      {message: "Não foi possivel buscar os eventos"},
+      {status: 500,}
+    )
+  }else{
+    const respData= await response.json();
+    return respData.event
+  }
+}
+export async function loadEvents(){
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // return { isError: true,message: 'não foi possivel  buscar os eventos'}
+    // throw new Response(
+    //                     JSON.stringify(
+    //                       {message: "Não foi possivel buscar os eventos"}
+    //                     ),
+    //                     {status: 500}
+    //                   )
+    throw json(
+                  {message: "Não foi possivel buscar os eventos"},
+                  {status: 500,}
+                )
+  } else {
+    const respData= await response.json();
+    return respData.events
+  }
+}
+
 export async function loader({request,params}){
   const id = params.id
-  const response = await fetch("http://localhost:8080/events/"+id)
-  if(!response.ok){
-    throw json({message: "não foi possivel buscar os dados deste evento em especifico"},{
-      status: 500
-    })
-  }else{
-    return response
-  }
-  
-
+  return defer({
+    event: await loadEvent(id),
+    events: loadEvents()
+  })
 }
 
 export async function action({params,request}){
